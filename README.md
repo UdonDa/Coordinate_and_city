7c0b8a2af3ffe2b21695753863d927cab12520ba
Udon <udoooon0727@gmail.com>
2017-10-18 10:30:34 +0900

    :sunflower:農研機構に変えた

--- README.md
+++ README.md
@@ -5,14 +5,13 @@
 
 ## 仕様技術
 [GoogleGeocofingAPI](https://developers.google.com/maps/documentation/geocoding/intro?hl=ja)
-
+[農研機構ジオコーディングサービス](https://www.finds.jp/rgeocode/index.html.ja)
 
 ## 環境
 Python 3.6.2  
 
 ## 使い方
 #### coordinate_to_city.py
-coordinateChanger = CoordinateChanger()  
-b = a.get_address("緯度", "経度")  
-(b = ○○市○町)  
+\# 農研機構  
+get_city(lat,lng)で、lat,lngの位置の区、市町村名を取得
 
--- src/coordinate_to_city.py
+++ src/coordinate_to_city.py
@@ -5,14 +5,15 @@ from urllib.parse import *
 import urllib
 import json
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 class CoordinateChanger():
     def __init__(self):
         self.G_APIKEY = "自分のAPI"
-        self.Y_APIKEY = "dj00aiZpPXFQV1VmSTRobjFjUSZzPWNvbnN1bWVyc2VjcmV0Jng9OWY-"
         self.G_base = "https://maps.googleapis.com/maps/api/geocode/json?language=ja"
-        self.Y_base = "https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?"
         self.G_sensor = "false"
+
+        self.N_base = "https://www.finds.jp/ws/rgeocode.php?json"
 
     def send_request(self, url):
         try:
@@ -29,18 +30,15 @@ class CoordinateChanger():
         #url += "&sensor={}".format(self.G_sensor)
         #url += "&key={}".format(self.G_APIKEY)
 
-        # Yahoo
-        url = "{}lat={}&lon={}".format(self.Y_base, lat, lng)
-        url += "&appid={}".format(self.Y_APIKEY)
-
-        # aaa = "https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?lat=35.68381981&lon=139.77456498&appid={}".format(self.Y_APIKEY)
+        # 農研機構
+        url = "{}&lat={}&lon={}".format(self.N_base, lat, lng)
         return url
 
     def change_coordinate_to_city(self, lat, lng):
         url = self.make_request_url(lat, lng)
         return self.send_request(url)
 
-    def get_address(self,lat, lng):
+    def get_response(self,lat, lng):
         # GoogleなんとかAPI -> JSON
         #JSON = self.change_coordinate_to_city(lat, lng).decode("utf-8")
         #data = json.loads(JSON)
@@ -48,30 +46,20 @@ class CoordinateChanger():
         #sub_address = data["results"][1]["address_components"][2]["long_name"]
         #return address+sub_address
 
-        #YahooなんとかAPI
-        url = self.make_request_url(lat, lng)
-        req = urllib.request.Request(url)
+        #農研機構
+        JSON = self.change_coordinate_to_city(lat,lng)
+        data = json.loads(JSON)
+        return data
 
-        with urllib.request.urlopen(req) as response:
-            XmlData = response.read()
-        return XmlData
+    def get_city(self, lat, lng):
+        JSONFILE = self.get_response(lat, lng)
+        values = JSONFILE['result']['municipality']['mname']
+        value = values.split(" ")
+        city_name = value[-1]
+        return city_name
 
-    def parse_xml(self, lat, lng):
-        xml_data = self.get_address(lat, lng)
-        result = ET.fromstring(xml_data)
-        return result
-
-
-
-
-def main():
-    coordinateChanger = CoordinateChanger()
-    lat = "34.7604130"
-    lng = "135.6269390"
-    XmlData = coordinateChanger.get_address(lat, lng)
-    root = ET.fromstring(XmlData)
-    return root
 
 a = CoordinateChanger()
-b = a.get_address("34.7604130", "135.6269390")
-print(b.decode("utf-8"))
+b = a.get_city("34.7604130", "135.6269390")
+print(b)
+
